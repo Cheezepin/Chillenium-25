15ff5618c0703f199500f64d8b74f977adb36a0d
Cheezepin <cheezepin@gmail.com>
2025-02-09 08:43:57 -0600

    intro, color wheel

--- scripts/ui/RotateSlides.cs
+++ scripts/ui/RotateSlides.cs
@@ -4,6 +4,9 @@ using System;
 public partial class RotateSlides : Control
 {
 	// Called when the node enters the scene tree for the first time.
+	public int currColor;
+	public int state = 0;
+	private double timer;
 	public override void _Ready()
 	{
 	}
@@ -11,14 +14,40 @@ public partial class RotateSlides : Control
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		float target = Global.currColorID*-60;
-		if(target == RotationDegrees) {
-			if(RotationDegrees < 0) RotationDegrees += 360f;
-			if(RotationDegrees >= 360.0f) RotationDegrees -= 360f;
-		} else {
-			
+		switch(state) {
+			case 0:
+				if(currColor != Global.currColorID) {
+					int neg = (((currColor - 1) + 12) % 6);
+					int pos = (currColor + 1) % 6;
+					while(neg != Global.currColorID && pos != Global.currColorID) {
+						neg--;
+						pos++;
+						neg += 12;
+						neg %= 6;
+						pos %= 6;
+					}
+					if(neg == Global.currColorID) {
+						state = 1;
+					} else {
+						state = 2;
+					}
+					timer = 0;
+				}
+				break;
+			case 1:
+			case 2:
+				RotationDegrees += 300f*((float)delta)*(state == 1 ? 1f : -1f);
+				if(timer >= 0.2) {
+					do {
+						currColor += (state == 1 ? -1 : 1) + 12;
+						currColor %= 6;
+					} while ((Global.colorsUnlocked & (1 << currColor)) == 0);
+					timer = 0;
+					state = 0;
+					RotationDegrees = 0;
+				}
+				timer += delta;
+				break;
 		}
-		// RotationDegrees = Mathf.MoveToward(RotationDegrees, target, 300f*(float)delta);
-		RotationDegrees = target;
 	}
 }
