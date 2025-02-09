5ca61fe0ee5815b7bc578a065f36979360a0c9ed
glotch <30377049+glotchalotch@users.noreply.github.com>
2025-02-09 10:08:50 -0600

    add sound on menu buttons

--- scripts/ui/Credits.cs
+++ scripts/ui/Credits.cs
@@ -7,6 +7,8 @@ public partial class Credits : Control
 	/* Credits Ready Function
 	Parameters: None
 	Description: Creates a back button and its event for when it is pressed */
+
+	[Export] public AudioStreamPlayer pressSound;
 	public override void _Ready(){
 		var backButton = (TextureButton)GetNode("Return");
 		var backButtonCaller = new Callable(this, nameof(OnBackButtonPressed));
@@ -15,6 +17,7 @@ public partial class Credits : Control
 
 	private void OnBackButtonPressed(){
 		Hide();
+		pressSound.Play();
 		GetParent().GetNode<Control>("MainMenuItems").Show();
 	}
 }
--- scripts/ui/MainMenu.cs
+++ scripts/ui/MainMenu.cs
@@ -5,10 +5,18 @@ public partial class MainMenu : Control
 {
 	
 	Credits creditsNode;
+	AudioStreamPlayer hoverSound;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		hoverSound = GetNode<AudioStreamPlayer>("Hover");
+		void Hover()
+        {
+			hoverSound.Play();
+        }
+		Callable hoverCallable = new Callable(this, nameof(Hover));
+
 		var playButton = (TextureButton)GetNode("MainMenuItems/Play");
 		var playButtonCaller = new Callable(this, nameof(OnPlayButtonPressed));
 		playButton.Connect("pressed", playButtonCaller);
@@ -29,15 +37,18 @@ public partial class MainMenu : Control
 	private void OnPlayButtonPressed(){
 		// Global.FadeToScene("res://levels/level1.tscn");
 		// Global.FadeToScene("res://ui/cutscenes/cutscene_1.tscn");
+		hoverSound.Play();
 		Global.transitions.ChangeLevel("SkyLevel");
 	}
 
 	private void OnCreditsButtonPressed(){
 		creditsNode.Show();
 		GetNode<Control>("MainMenuItems").Hide();
+		hoverSound.Play();
 	}
 
 	private void OnQuitButtonPressed(){
+		hoverSound.Play();
 		GetTree().Quit();
 	}
 }
